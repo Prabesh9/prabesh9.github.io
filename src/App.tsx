9c6948dc25d59bcc00bc114781632b4af3bf686b
Prabesh Maharjan <prabeshmrz.pm@gmail.com>
2021-07-03 22:00:59 +0545

    + typescript

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to my portfolio!</h1>
+        <h1>Welcome to my portfolio!</h1>
     </div>
   );
 }
